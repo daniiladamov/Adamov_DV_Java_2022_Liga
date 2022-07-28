680e3c2c05e8a86e2c6b2c81ab1988d1a4f6879a
d.adamov <adamovdaniil@yandex.ru>
2022-07-28 15:38:24 +0300

    глобальный обработчик исключений в контроллерах

--- src/main/java/homework/controller/СustomControllerAdvice.java
+++ src/main/java/homework/controller/СustomControllerAdvice.java
@@ -1,6 +1,7 @@
 package homework.controller;
 
 import homework.exception.EntityNotFoundException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -8,28 +9,33 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @ControllerAdvice
-public class СustomControllerAdvice{
+@RestController
+public class СustomControllerAdvice extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler({MethodArgumentNotValidException.class})
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public Map<String, String> totValidResponse(MethodArgumentNotValidException ex){
+
+    @ExceptionHandler({EntityNotFoundException.class})
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String objectNotFoundResponse(EntityNotFoundException ex){
+        return ex.getMessage();
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
         Map<String, String> errors = new HashMap<>();
         ex.getBindingResult().getAllErrors().forEach((error) -> {
             String fieldName = ((FieldError) error).getField();
             String errorMessage = error.getDefaultMessage();
             errors.put(fieldName, errorMessage);
         });
-        return errors;
-    }
-
-    @ExceptionHandler({EntityNotFoundException.class})
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public String objectNotFoundResponse(EntityNotFoundException ex){
-        return ex.getMessage();
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 }

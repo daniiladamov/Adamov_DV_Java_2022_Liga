72a9f9d1294eedd52328c76c2bf5a9fe3b4498fc
daniiladamov <daniiladamov@mail.ru>
2022-07-30 15:42:29 +0300

    basic auth

--- src/main/java/homework/config/SecurityConfig.java
+++ src/main/java/homework/config/SecurityConfig.java
@@ -1,18 +1,40 @@
 package homework.config;
 
-import homework.security.AuthProviderImpl;
+import homework.service.CustomUserDetailsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final AuthProviderImpl authProvider;
+   private final CustomUserDetailsService detailsService;
 
-    protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder){
-        authenticationManagerBuilder.authenticationProvider(authProvider);
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(detailsService);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.
+                csrf().disable().
+                authorizeRequests().
+                antMatchers("/v2/users").permitAll().
+                anyRequest().authenticated().
+                and().
+                formLogin().
+                and().
+                httpBasic();
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
     }
 }

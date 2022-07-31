d2f4bb305afa409223b7ddb73ed6f8570e2d554e
daniiladamov <daniiladamov@mail.ru>
2022-07-31 13:48:33 +0300

    замечание от Дениса (3)

--- src/main/java/homework/controller/UserController.java
+++ src/main/java/homework/controller/UserController.java
@@ -8,27 +8,23 @@ import homework.entity.user.User;
 import homework.entity.user.UserGetDto;
 import homework.entity.user.UserSaveDto;
 import homework.exception.EntityNotFoundException;
-import homework.exception.LoginAlreadyUsedException;
 import homework.service.RelationService;
 import homework.service.UserService;
 import homework.util.CustomPage;
-import homework.util.UserValidator;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
 import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/v2/users")
 public class UserController {
-    private final UserValidator userValidator;
     private final RelationService relationService;
     private final UserService userService;
     private final ModelMapper modelMapper;
@@ -55,9 +51,7 @@ public class UserController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Long createUser(@Valid @RequestBody UserSaveDto userSaveDto, BindingResult bindingResult)
-            throws LoginAlreadyUsedException {
-        userValidator.validate(userSaveDto, bindingResult);
+    public Long createUser(@Validated @RequestBody UserSaveDto userSaveDto){
         User user = modelMapper.map(userSaveDto, User.class);
         return userService.createUser(user);
     }
@@ -74,7 +68,7 @@ public class UserController {
     }
 
     @PutMapping("/{id}")
-    public UserGetDto updateUser(@Valid @RequestBody UserSaveDto userSaveDto, @PathVariable Long id) {
+    public UserGetDto updateUser(@Validated @RequestBody UserSaveDto userSaveDto, @PathVariable Long id) {
         User user = modelMapper.map(userSaveDto, User.class);
         user.setId(id);
         User userUpdate = userService.updateUser(user);
